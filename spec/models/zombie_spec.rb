2b2903dc7bd6d78cfb0c62eb3d055c26a43d0e94
Kerem Bozdas <krmbzds.github@gmail.com>
2014-12-05 11:52:36 +0200

    Refactor example w/ implicit receiver & subject

--- spec/models/zombie_spec.rb
+++ spec/models/zombie_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Zombie, :type => :model do
 
-  it 'is invalid without a name' do
-    zombie = Zombie.new
-    expect(zombie).not_to be_valid
-  end
+  # Refactor test using implicit subject
+  it { should_not be_valid }
+
+  # it 'is invalid without a name' do
+  #   zombie = Zombie.new
+  #   expect(zombie).not_to be_valid
+  # end
 
   it "has a name that matches 'Tyrone Hayes'" do
     zombie = Zombie.new(:name => 'Tyrone 420 Hayes')
