083e36dd757822c19af93571aa65cf433fb4ceb7
Kerem Bozdas <krmbzds.github@gmail.com>
2014-12-05 11:12:27 +0200

    Tyrone finally got his shit together

--- spec/models/zombie_spec.rb
+++ spec/models/zombie_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Zombie, :type => :model do
     expect(zombie).not_to be_valid
   end
 
+  it "has a name that matches 'Tyrone Hayes'" do
+    zombie = Zombie.new(:name => 'Tyrone 420 Hayes')
+    expect(zombie.name).to match(/Tyrone \d\d\d Hayes/)
+  end
 
 end
